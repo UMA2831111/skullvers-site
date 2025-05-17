f8aef3be7e71f2086e1ff4d95eb1af2a79ed400d
UMA2831111 <imeshpathirana1@gmail.com>
2025-05-17 20:50:16 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,2 @@
+import SkullversSite from './SkullversSite';
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import SkullversSite from './SkullversSite'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <SkullversSite />
-  </React.StrictMode>
-)
